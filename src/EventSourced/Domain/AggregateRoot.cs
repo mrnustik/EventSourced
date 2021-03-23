d8bc0f6acb29f417c08584cbb734ca981a4d56d1
Michal Mrnuštík <michal.mrnustik@riganti.cz>
2021-03-23 20:48:20 +0100

    Add base class for aggregate root

---
+++ src/EventSourced/Domain/AggregateRoot.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using EventSourced.Domain.Events;
+using EventSourced.Helpers;
+
+namespace EventSourced.Domain
+{
+    public abstract class AggregateRoot
+    {
+        private readonly Queue<IDomainEvent> uncommittedDomainEvents;
+
+        protected AggregateRoot()
+        {
+            uncommittedDomainEvents = new Queue<IDomainEvent>();
+        }
+
+        public IList<IDomainEvent> DequeueDomainEvents()
+        {
+            return uncommittedDomainEvents.ToList();
+        }
+
+        protected void EnqueueAndApplyEvent(IDomainEvent domainEvent)
+        {
+            uncommittedDomainEvents.Enqueue(domainEvent);
+            TryToApplyEvent(domainEvent);
+        }
+
+        private void TryToApplyEvent(IDomainEvent domainEvent)
+        {
+            var applyMethod = ReflectionHelpers.GetApplyMethodForEventInAggregateRoot(this, domainEvent);
+            if (applyMethod != null)
+            {
+                applyMethod.Invoke(this, new[] {domainEvent});
+            }
+            else
+            {
+                throw new ArgumentException(
+                    $"Missing Apply event for domain event of type {domainEvent.GetType()} on aggregate {GetType()}");
+            }
+        }
+    }
+}
