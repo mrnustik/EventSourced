1dcdb6dc83ff285b0f47721ec04cf8d7aaaa107d
Michal Mrnuštík <michal.mrnustik@riganti.cz>
2021-03-23 21:07:33 +0100

    Fix aggregate root still has its events after dequeuing

---
+++ src/EventSourced/Helpers/QueueExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace EventSourced.Helpers
+{
+    internal static class QueueExtensions
+    {
+        public static IList<TItem> DequeueAll<TItem>(this Queue<TItem> queue)
+        {
+            var list = new List<TItem>();
+            while (queue.TryDequeue(out var result))
+            {
+                list.Add(result);
+            }
+            return list;
+        }
+    }
+}
