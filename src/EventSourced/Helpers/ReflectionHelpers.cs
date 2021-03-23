6fda5897657b7e818c9cc74073384b651f1ba701
Michal Mrnuštík <michal.mrnustik@riganti.cz>
2021-03-23 20:48:41 +0100

    Add tests for aggregate root base class domain access

---
+++ src/EventSourced/Helpers/ReflectionHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Reflection;
+using EventSourced.Domain;
+using EventSourced.Domain.Events;
+
+namespace EventSourced.Helpers
+{
+    public static class ReflectionHelpers
+    {
+        public static MethodInfo? GetApplyMethodForEventInAggregateRoot(AggregateRoot root, IDomainEvent domainEvent)
+        {
+            var aggregateType = root.GetType();
+            var methods = aggregateType.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            return methods
+                .Where(m => m.Name == "Apply")
+                .SingleOrDefault(m =>
+                    m.GetParameters().Length == 1 && m.GetParameters()[0].ParameterType == domainEvent.GetType());
+        }
+    }
+}
