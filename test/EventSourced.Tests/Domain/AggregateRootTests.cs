8b022ee903b23ac1d09bad50464b02bd0b6befa4
Michal Mrnuštík <michal.mrnustik@riganti.cz>
2021-03-23 20:48:41 +0100

    Add tests for aggregate root base class domain access

---
+++ test/EventSourced.Tests/Domain/AggregateRootTests.cs
@@ -0,0 +1,50 @@
+﻿using EventSourced.Tests.TestDoubles.AggregateRoots;
+using EventSourced.Tests.TestDoubles.DomainEvents;
+using FluentAssertions;
+using Xunit;
+
+namespace EventSourced.Tests.Domain
+{
+    public class AggregateRootTests
+    {
+        [Fact]
+        public void EnqueuedDomainEvent_IsAddedToQueue_And_CanBeDequeued()
+        {
+            //Arrange
+            var sut = CreateTestAggregateRootWithApplyForTestEvent();
+            const string testParameterValue = "Test value";
+
+            //Act
+            sut.EnqueueTestDomainEvent(testParameterValue);
+
+            //Assert
+            var uncommittedDomainEvents = sut.DequeueDomainEvents();
+            uncommittedDomainEvents
+                .Should()
+                .HaveCount(1)
+                .And
+                .ContainSingle(e => e.As<TestDomainEvent>().Parameter == testParameterValue);
+        }
+        
+        [Fact]
+        public void EnqueuedDomainEvent_AppliesTheEventToAggregate()
+        {
+            //Arrange
+            var sut = CreateTestAggregateRootWithApplyForTestEvent();
+            const string testParameterValue = "Test value";
+
+            //Act
+            sut.EnqueueTestDomainEvent(testParameterValue);
+
+            //Assert
+            sut.ParameterValue
+                .Should()
+                .Be(testParameterValue);
+        }
+
+        private TestAggregateRootWithApplyForTestEvent CreateTestAggregateRootWithApplyForTestEvent()
+        {
+            return new TestAggregateRootWithApplyForTestEvent();
+        }
+    }
+}
---
+++ test/EventSourced.Tests/TestDoubles/AggregateRoots/TestAggregateRootWithApplyForTestEvent.cs
@@ -0,0 +1,21 @@
+﻿using EventSourced.Domain;
+using EventSourced.Tests.TestDoubles.DomainEvents;
+
+namespace EventSourced.Tests.TestDoubles.AggregateRoots
+{
+    public class TestAggregateRootWithApplyForTestEvent : AggregateRoot
+    {
+        public string ParameterValue { get; private set; } = string.Empty;
+        
+        public void EnqueueTestDomainEvent(string parameter)
+        {
+            var domainEvent = new TestDomainEvent(parameter);
+            EnqueueAndApplyEvent(domainEvent);
+        }
+
+        public void Apply(TestDomainEvent domainEvent)
+        {
+            ParameterValue = domainEvent.Parameter;
+        }
+    }
+}
