7c23554f3ffda6e03a33ec798ad2b5bc5c3267ef
Michal Mrnuštík <michal.mrnustik@riganti.cz>
2021-03-28 22:36:37 +0200

    Add basic repository implementation

---
+++ src/EventSourced.Persistence.Abstractions/IEventStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using EventSourced.Abstractions.Domain.Events;
+
+namespace EventSourced.Persistence.Abstractions
+{
+    public interface IEventStore
+    {
+        Task StoreEventsAsync(IList<IDomainEvent> domainEvents, CancellationToken ct);
+        Task<IList<IDomainEvent>> GetByStreamIdAsync(Guid streamId, CancellationToken ct);
+    }
+}
