0c47c35d37e5760722c5035b786773d204560735
Michal Mrnuštík <michal.mrnustik@riganti.cz>
2021-04-12 13:27:31 +0200

    Add in memory event store

---
+++ src/EventSourced.Persistence.InMemory/InMemoryEventStore.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EventSourced.Abstractions.Domain.Events;
+using EventSourced.Persistence.Abstractions;
+
+namespace EventSourced.Persistence.InMemory
+{
+    public class InMemoryEventStore : IEventStore
+    {
+        private ConcurrentDictionary<string, List<IDomainEvent>> StreamsDictionary { get; }
+
+        public InMemoryEventStore()
+            : this(new Dictionary<string, List<IDomainEvent>>())
+        {
+        }
+        
+        public InMemoryEventStore(IDictionary<string, List<IDomainEvent>> originalState)
+        {
+            StreamsDictionary = new ConcurrentDictionary<string, List<IDomainEvent>>(originalState);
+        }
+
+        public Task StoreEventsAsync(string streamId, IList<IDomainEvent> domainEvents, CancellationToken ct)
+        {
+            StreamsDictionary.AddOrUpdate(streamId,
+                _ => domainEvents.ToList(),
+                (_, existingEvents) => existingEvents.Concat(domainEvents).ToList());
+            return Task.CompletedTask;
+        }
+
+        public Task<IDomainEvent[]> GetByStreamIdAsync(string streamId, CancellationToken ct)
+        {
+            if (StreamsDictionary.TryGetValue(streamId, out var events))
+            {
+                var eventsArray = events.ToArray();
+                return Task.FromResult(eventsArray);
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
