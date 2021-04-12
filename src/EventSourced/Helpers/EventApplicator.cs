7533dcfeebcf7d8ba70f5237de9ef0cf64cee61d
Michal Mrnuštík <michal.mrnustik@riganti.cz>
2021-04-12 12:42:28 +0200

    Extract event application logic to its own class

---
+++ src/EventSourced/Helpers/EventApplicator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using EventSourced.Abstractions.Domain.Events;
+
+namespace EventSourced.Helpers
+{
+    internal static class EventApplicator
+    {
+        public static void ApplyEventsToObject(this object @object, params IDomainEvent[] domainEvents)
+        {
+            foreach (var domainEvent in domainEvents)
+            {
+                var applyMethod = ReflectionHelpers.GetApplyMethodForEventInObject(@object, domainEvent);
+                if (applyMethod != null)
+                {
+                    applyMethod.Invoke(@object, new[] {domainEvent});
+                }
+                else
+                {
+                    throw new ArgumentException(
+                        $"Missing Apply event for domain event of type {domainEvent.GetType()} on object {@object.GetType()}");
+                }
+            }
+        }
+    }
+}
