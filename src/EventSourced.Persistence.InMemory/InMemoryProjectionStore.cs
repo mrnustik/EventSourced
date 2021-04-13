01d87e1d126b69d10cd114eef11c88fadca6e317
Michal Mrnuštík <michal.mrnustik@riganti.cz>
2021-04-13 22:37:02 +0200

    Add InMemoryProjectionStore

---
+++ src/EventSourced.Persistence.InMemory/InMemoryProjectionStore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventSourced.Persistence.InMemory
+{
+    public class InMemoryProjectionStore : IProjectionStore
+    {
+        private ConcurrentDictionary<Type, object> ProjectionsMap { get; }
+
+        public InMemoryProjectionStore() : this(new ConcurrentDictionary<Type, object>())
+        {
+        }
+
+        public InMemoryProjectionStore(ConcurrentDictionary<Type, object> projectionsMap)
+        {
+            ProjectionsMap = projectionsMap;
+        }
+        
+        public Task<object?> LoadProjectionAsync(Type projectionType, CancellationToken ct)
+        {
+            var projection = ProjectionsMap.GetValueOrDefault(projectionType);
+            return Task.FromResult(projection);
+        }
+
+        public Task StoreProjectionAsync(object projection, CancellationToken ct)
+        {
+            ProjectionsMap[projection.GetType()] = projection;
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ test/EventSourced.Tests/Persistence/InMemory/InMemoryProjectionStoreTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using EventSourced.Persistence;
+using EventSourced.Persistence.InMemory;
+using FluentAssertions;
+using Xunit;
+
+namespace EventSourced.Tests.Persistence.InMemory
+{
+    public class InMemoryProjectionStoreTests
+    {
+        [Fact]
+        public async Task StoreProjectionAsync_WithValidValue_CanBeThenLoaded()
+        {
+            //Arrange
+            var projection = new TestProjection(42); 
+            var sut = CreateSut();
+            
+            //Act
+            await sut.StoreProjectionAsync(projection, CancellationToken.None);
+
+            //Assert
+            var loadedProjection = await sut.LoadProjectionAsync<TestProjection>(CancellationToken.None);
+            loadedProjection!
+                .Number
+                .Should()
+                .Be(42);
+        }
+
+        [Fact]
+        public async Task LoadProjectionAsync_WithNonExistingType_ReturnsNull()
+        {
+            //Arrange
+            var sut = CreateSut();
+            
+            //Act
+            var loadedProjection = await sut.LoadProjectionAsync<TestProjection>(CancellationToken.None);
+
+            //Assert
+            loadedProjection
+                .Should()
+                .BeNull();
+        }
+        
+        [Fact]
+        public async Task LoadProjectionAsync_WithExistingType_ReturnsIt()
+        {
+            //Arrange
+            var projection = new TestProjection(42); 
+            var sut = CreateSut(new Dictionary<Type, object>
+            {
+                {typeof(TestProjection), projection}
+            });
+            
+            //Act
+            var loadedProjection = await sut.LoadProjectionAsync<TestProjection>(CancellationToken.None);
+
+            //Assert
+            loadedProjection
+                .Number
+                .Should()
+                .Be(42);
+        }
+
+        private IProjectionStore CreateSut()
+        {
+            return new InMemoryProjectionStore();
+        }
+        
+        private IProjectionStore CreateSut(Dictionary<Type,object> originalState)
+        {
+            return new InMemoryProjectionStore(new ConcurrentDictionary<Type, object>(originalState));
+        }
+        
+        private class TestProjection
+        {
+            public int Number { get; }
+
+            public TestProjection(int number)
+            {
+                Number = number;
+            }
+        }
+    }
+}
