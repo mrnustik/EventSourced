713309a29487310fbdd815a3dae4e209fee91836
Michal Mrnuštík <michal.mrnustik@riganti.cz>
2021-04-13 18:51:31 +0200

    Add automatic projection registration via extension methods

--- src/EventSourced/Configuration/EventSourcedOptions.cs
+++ src/EventSourced/Configuration/EventSourcedOptions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace EventSourced.Configuration
 {
@@ -12,5 +14,11 @@ namespace EventSourced.Configuration
             ServiceCollection = serviceCollection;
             AutomaticProjectionOptions = new AutomaticProjectionOptions();
         }
+    }
+
+    public class AutomaticProjectionOptions
+    {
+        public ICollection<Type> RegisteredAutomaticProjections { get; } = new List<Type>();
+        public ICollection<Type> RegisteredAutomaticAggregateProjections { get; } = new List<Type>();
     }
 }
