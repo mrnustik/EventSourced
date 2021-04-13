3f402000edf3430b1d1bc8296c509f30c46532cf
Michal Mrnuštík <michal.mrnustik@riganti.cz>
2021-04-13 22:37:02 +0200

    Add InMemoryProjectionStore

--- src/EventSourced.Persistence.InMemory/Configuration/EventSourcedOptionsExtensions.cs
+++ src/EventSourced.Persistence.InMemory/Configuration/EventSourcedOptionsExtensions.cs
@@ -5,9 +5,16 @@ namespace EventSourced.Persistence.InMemory.Configuration
 {
     public static class EventSourcedOptionsExtensions
     {
-        public static void UseInMemoryEventStore(this EventSourcedOptions options)
+        public static EventSourcedOptions UseInMemoryEventStore(this EventSourcedOptions options)
         {
             options.ServiceCollection.AddSingleton<IEventStore, InMemoryEventStore>();
+            return options;
+        }
+        
+        public static EventSourcedOptions UseInMemoryProjectionStore(this EventSourcedOptions options)
+        {
+            options.ServiceCollection.AddSingleton<IProjectionStore, InMemoryProjectionStore>();
+            return options;
         }
     }
 }
---
+++ src/EventSourced/Persistence/IProjectionStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventSourced.Persistence
+{
+    public interface IProjectionStore
+    {
+        public async Task<TProjection?> LoadProjectionAsync<TProjection>(CancellationToken ct) =>
+            (TProjection?) await LoadProjectionAsync(typeof(TProjection), ct);
+        Task<object?> LoadProjectionAsync(Type projectionType,CancellationToken ct);
+        Task StoreProjectionAsync(object projection, CancellationToken ct);
+    }
+}
