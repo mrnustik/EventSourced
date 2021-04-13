a56576cd445b5fb35a2be96ed16f01c92ea46624
Michal Mrnuštík <michal.mrnustik@riganti.cz>
2021-04-13 13:13:45 +0200

    Add configuration of EventSourced project

---
+++ src/EventSourced/Configuration/EventSourcedOptions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace EventSourced.Configuration
+{
+    public class EventSourcedOptions
+    {
+        public IServiceCollection ServiceCollection { get; }
+
+        public EventSourcedOptions(IServiceCollection serviceCollection)
+        {
+            ServiceCollection = serviceCollection;
+        }
+    }
+}
---
+++ src/EventSourced/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using EventSourced.Persistence;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventSourced.Configuration
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddEventSourced(this IServiceCollection serviceCollection, Action<EventSourcedOptions> optionsConfiguration)
+        {
+            serviceCollection.AddTransient(typeof(IRepository<,>), typeof(Repository<,>));
+
+            var options = new EventSourcedOptions(serviceCollection);
+            optionsConfiguration(options);
+            serviceCollection.AddSingleton(options);
+        }
+    }
+}
