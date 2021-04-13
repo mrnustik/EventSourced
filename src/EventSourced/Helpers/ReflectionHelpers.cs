063b92694f5fa2214359551878aa4b4032319b70
Michal Mrnuštík <michal.mrnustik@riganti.cz>
2021-04-13 15:36:00 +0200

    Add method for applicable events lookup to ReflectionHelpers

--- src/EventSourced/Helpers/ReflectionHelpers.cs
+++ src/EventSourced/Helpers/ReflectionHelpers.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using EventSourced.Abstractions.Domain.Events;
 using EventSourced.Domain;
@@ -6,16 +8,28 @@ using EventSourced.Domain.Events;
 
 namespace EventSourced.Helpers
 {
-    public static class ReflectionHelpers
+    internal static class ReflectionHelpers
     {
+        private const BindingFlags DefaultApplyMethodBindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
+
         public static MethodInfo? GetApplyMethodForEventInObject(object @object, IDomainEvent domainEvent)
         {
             var aggregateType = @object.GetType();
-            var methods = aggregateType.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            var methods = aggregateType.GetMethods(DefaultApplyMethodBindingFlags);
             return methods
                 .Where(m => m.Name == "Apply")
                 .SingleOrDefault(m =>
                     m.GetParameters().Length == 1 && m.GetParameters()[0].ParameterType == domainEvent.GetType());
+        }
+
+        public static IEnumerable<Type> GetTypesOfDomainEventsApplicableToObject(Type objectType)
+        {
+            return objectType.GetMethods(DefaultApplyMethodBindingFlags)
+                .Where(m => m.Name.Equals("Apply"))
+                .Where(m => m.GetParameters().Length == 1)
+                .SelectMany(m => m.GetParameters())
+                .Where(p => p.ParameterType.IsAssignableTo(typeof(IDomainEvent)))
+                .Select(p => p.ParameterType);
         }
     }
 }
