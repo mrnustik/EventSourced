933e91f8dd17a6677b3e97404476a2a482d9aa60
Michal Mrnuštík <michal.mrnustik@riganti.cz>
2021-04-13 22:40:48 +0200

    Add DomainEventHandlers for listening to stored domain events

---
+++ src/EventSourced/Domain/Events/IDomainEventHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventSourced.Domain.Events
+{
+    public interface IDomainEventHandler
+    {
+        Task HandleDomainEventAsync(IDomainEvent domainEvent, CancellationToken ct);
+    }
+}
