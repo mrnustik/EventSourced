449f8866633484a27fdbdc3cfd963dbc3510c0fa
Michal Mrnuštík <michal.mrnustik@riganti.cz>
2021-04-13 13:13:45 +0200

    Add configuration of EventSourced project

---
+++ src/EventSourced.Persistence.InMemory/Configuration/EventSourcedOptionsExtensions.cs
@@ -0,0 +1,14 @@
+﻿using EventSourced.Configuration;
+using EventSourced.Persistence.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventSourced.Persistence.InMemory.Configuration
+{
+    public static class EventSourcedOptionsExtensions
+    {
+        public static void UseInMemoryEventStore(this EventSourcedOptions options)
+        {
+            options.ServiceCollection.AddSingleton<IEventStore, InMemoryEventStore>();
+        }
+    }
+}
