f8f1a0bf5c3a398ae3fb8681cf7026d08b10e9ce
Michal Mrnuštík <michal.mrnustik@riganti.cz>
2021-04-13 14:49:42 +0200

    Add accessing for all warehouse items in sample application

---
+++ samples/EventSourced.Sample.Warehouse.Application/Model/WarehouseLisItemModel.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace EventSourced.Sample.Warehouse.Application.Model
+{
+    public record WarehouseLisItemModel(Guid Id, string Title);
+}
---
+++ samples/EventSourced.Sample.Warehouse.Application/Services/WarehouseItem/IGetAllWarehouseItemsApplicationService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using EventSourced.Sample.Warehouse.Application.Model;
+
+namespace EventSourced.Sample.Warehouse.Application.Services.WarehouseItem
+{
+    public interface IGetAllWarehouseItemsApplicationService
+    {
+        Task<ICollection<WarehouseLisItemModel>> GetAllAsync(CancellationToken ct);
+    }
+}
