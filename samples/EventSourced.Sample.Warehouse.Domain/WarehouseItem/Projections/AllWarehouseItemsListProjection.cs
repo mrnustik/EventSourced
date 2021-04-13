16e95a421a6e12ae17f715e17b8d64d111f44b5f
Michal Mrnuštík <michal.mrnustik@riganti.cz>
2021-04-13 16:09:21 +0200

    Use manual projection instead of Repository.GetAll when loading all warehouse items

---
+++ samples/EventSourced.Sample.Warehouse.Domain/WarehouseItem/Projections/AllWarehouseItemsListProjection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using EventSourced.Sample.Warehouse.Domain.WarehouseItem.Events;
+
+namespace EventSourced.Sample.Warehouse.Domain.WarehouseItem.Projections
+{
+    public class AllWarehouseItemsListProjection
+    {
+        public record WarehouseListItem(Guid Id, string Title);
+        public ICollection<WarehouseListItem> Items { get; } = new List<WarehouseListItem>();
+
+        public void Apply(WarehouseItemCreatedDomainEvent @event)
+        {
+            Items.Add(new WarehouseListItem(@event.WarehouseItemId, @event.Title));
+        } 
+    }
+}
