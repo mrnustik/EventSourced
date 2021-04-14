43c00ed514268fad53df20914a4159333e8cba99
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-14 18:36:12 +0200

    Add NullSnapshotCreationStrategy

---
+++ test/EventSourced.Tests/Snapshots/Strategies/NullSnapshotCreationStrategyTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using EventSourced.Domain;
+using EventSourced.Snapshots;
+using EventSourced.Snapshots.Strategies;
+using FluentAssertions;
+using Xunit;
+
+namespace EventSourced.Tests.Snapshots.Strategies
+{
+    public class NullSnapshotCreationStrategyTests
+    {
+        [Fact]
+        public void ShouldCreateSnapshot_AlwaysReturnsFalse()
+        {
+            //Arrange
+            var aggregateRoot = new TestAggregateRoot(Guid.NewGuid());
+            var sut = CreateSut();
+            
+            //Act
+            var shouldCreateSnapshot = sut.ShouldCreateSnapshot(aggregateRoot);
+
+            //Assert
+            shouldCreateSnapshot.Should()
+                                .BeFalse();
+        }
+
+        private ISnapshotCreationStrategy CreateSut()
+        {
+            return new NullSnapshotCreationStrategy();
+        }
+        
+        private class TestAggregateRoot : AggregateRoot
+        {
+            public TestAggregateRoot(Guid id)
+                : base(id)
+            {
+            }
+        }
+    }
+}
