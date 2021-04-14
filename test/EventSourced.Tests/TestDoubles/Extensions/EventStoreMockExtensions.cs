c0eda53106bd401608e1d7a1a27b7d83c40fa8e6
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-14 15:18:55 +0200

    Add event conflict handling

---
+++ test/EventSourced.Tests/TestDoubles/Extensions/EventStoreMockExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using EventSourced.Domain.Events;
+using EventSourced.Persistence;
+using Moq;
+
+namespace EventSourced.Tests.TestDoubles.Extensions
+{
+    public static class EventStoreMockExtensions
+    {
+        public static Mock<IEventStore> WithStreamExistsAsync(this Mock<IEventStore> mock, bool result)
+        {
+            mock
+                .Setup(s => s.StreamExistsAsync(It.IsAny<Guid>(), It.IsAny<Type>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(result);
+            return mock;
+        }
+
+        public static Mock<IEventStore> WithGetByStreamIdAsync(this Mock<IEventStore> mock,
+                                                               Guid streamId,
+                                                               IEnumerable<IDomainEvent> domainEvents)
+        {
+            mock
+                .Setup(s => s.GetByStreamIdAsync(streamId, It.IsAny<Type>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(domainEvents.ToArray());
+            return mock;
+        }
+
+        public static Mock<IEventStore> WithGetAllStreamsOfType(this Mock<IEventStore> mock,
+                                                                IDictionary<Guid, IDomainEvent[]> aggregateToEventsMap)
+        {
+            mock
+                .Setup(s => s.GetAllStreamsOfType(It.IsAny<Type>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(aggregateToEventsMap);
+            return mock;
+        }
+    }
+}
