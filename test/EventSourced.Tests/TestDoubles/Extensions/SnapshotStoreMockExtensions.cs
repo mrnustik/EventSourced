df3da1e37841522749530188bededa7ed67651d9
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-14 17:48:35 +0200

    Add test for aggregate being loaded from snapshot when accessing repository

---
+++ test/EventSourced.Tests/TestDoubles/Extensions/SnapshotStoreMockExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using EventSourced.Domain;
+using EventSourced.Persistence;
+using Moq;
+
+namespace EventSourced.Tests.TestDoubles.Extensions
+{
+    public static class SnapshotStoreMockExtensions
+    {
+        public static Mock<ISnapshotStore<TAggregateRoot>> WithLoadSnapshotAsync<TAggregateRoot>(
+            this Mock<ISnapshotStore<TAggregateRoot>> mock,
+            TAggregateRoot aggregateRoot) 
+            where TAggregateRoot : AggregateRoot
+        {
+            mock.Setup(s => s.LoadSnapshotAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(aggregateRoot);
+            return mock;
+        }
+    }
+}
