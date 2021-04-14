ee90287d1f04e45d8b7cd28b6f21f65e182d757c
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-14 12:09:10 +0200

    MAke EventSourcedOptionsExtensions use Fluent API

--- src/EventSourced/Configuration/EventSourcedOptionsExtensions.cs
+++ src/EventSourced/Configuration/EventSourcedOptionsExtensions.cs
@@ -5,19 +5,21 @@ namespace EventSourced.Configuration
 {
     public static class EventSourcedOptionsExtensions
     {
-        public static void RegisterAutomaticProjection<TProjection>(this EventSourcedOptions options)
+        public static EventSourcedOptions RegisterAutomaticProjection<TProjection>(this EventSourcedOptions options)
             where TProjection : new()
         {
             options.AutomaticProjectionOptions.RebuildAutomaticProjectionsOnStart = true;
             options.AutomaticProjectionOptions.RegisteredAutomaticProjections.Add(typeof(TProjection));
+            return options;
         }
         
-        public static void RegisterAutomaticAggregateProjection<TProjection, TAggregateRoot>(this EventSourcedOptions options)
+        public static EventSourcedOptions RegisterAutomaticAggregateProjection<TProjection, TAggregateRoot>(this EventSourcedOptions options)
             where TProjection : AggregateProjection<TAggregateRoot>
             where TAggregateRoot : AggregateRoot
         {
             options.AutomaticProjectionOptions.RebuildAutomaticProjectionsOnStart = true;
             options.AutomaticProjectionOptions.RegisteredAutomaticAggregateProjections.Add(typeof(TProjection));
+            return options;
         }
     }
 }
