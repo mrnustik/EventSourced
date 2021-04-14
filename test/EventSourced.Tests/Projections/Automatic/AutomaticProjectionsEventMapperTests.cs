3af53e71e0529a860c1c10115d0a80a2c7730996
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-14 12:15:45 +0200

    Add test for mapping of aggregates to projection in AutomaticProjectionsEventMapperTests

--- test/EventSourced.Tests/Projections/Automatic/AutomaticProjectionsEventMapperTests.cs
+++ test/EventSourced.Tests/Projections/Automatic/AutomaticProjectionsEventMapperTests.cs
@@ -1,6 +1,8 @@
 using System;
 using EventSourced.Configuration;
+using EventSourced.Domain;
 using EventSourced.Domain.Events;
+using EventSourced.Projections;
 using EventSourced.Projections.Automatic;
 using FluentAssertions;
 using Xunit;
@@ -63,7 +65,7 @@ namespace EventSourced.Tests.Projections.Automatic
         }
         
         [Fact]
-        public void GetProjectionsAffectedByEvent_WithInitializedEvent_ReturnsEmpty()
+        public void GetProjectionsAffectedByEvent_WithInitializedEvent_ReturnsIt()
         {
             //Arrange
             var sut = CreateSut(CreateOptionsWithValidProjection());
@@ -78,6 +80,22 @@ namespace EventSourced.Tests.Projections.Automatic
                 .Contain(typeof(ProjectionReactingToEmptyEvent));
         }
         
+        [Fact]
+        public void GetProjectionsAffectedByAggregateChange_WithInitializedAggregateProjection_ReturnsEmpty()
+        {
+            //Arrange
+            var sut = CreateSut(CreateOptionsWithValidProjection());
+            sut.Initialize();
+            
+            //Act
+            var projectionTypes = sut.GetProjectionsAffectedByAggregateChange(typeof(TestAggregateRoot));
+
+            //Assert
+            projectionTypes
+                .Should()
+                .Contain(typeof(TestAggregateProjection));
+        }
+        
         private AutomaticProjectionOptions CreateOptionsWithEmptyProjection()
         {
             var options = new AutomaticProjectionOptions();
@@ -89,6 +107,7 @@ namespace EventSourced.Tests.Projections.Automatic
         {
             var options = new AutomaticProjectionOptions();
             options.RegisteredAutomaticProjections.Add(typeof(ProjectionReactingToEmptyEvent));
+            options.RegisteredAutomaticAggregateProjections.Add(typeof(TestAggregateProjection));
             return options;
         }
 
@@ -109,6 +128,24 @@ namespace EventSourced.Tests.Projections.Automatic
         private class ProjectionReactingToEmptyEvent
         {
             private void Apply(EmptyEvent emptyEvent)
+            {
+            }
+        }
+        
+        private class TestAggregateProjection : AggregateProjection<TestAggregateRoot>
+        {
+            public TestAggregateProjection(Guid id) : base(id)
+            {
+            }
+            
+            private void Apply(EmptyEvent emptyEvent)
+            {
+            }
+        }
+
+        private class TestAggregateRoot : AggregateRoot
+        {
+            public TestAggregateRoot(Guid id) : base(id)
             {
             }
         }
