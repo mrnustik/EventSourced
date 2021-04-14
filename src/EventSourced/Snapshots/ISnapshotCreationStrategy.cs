49a44317f0d00541545cdd2f9523404770157b9c
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-14 18:35:26 +0200

    Add snapshot creation strategies

---
+++ src/EventSourced/Snapshots/ISnapshotCreationStrategy.cs
@@ -0,0 +1,10 @@
+﻿using EventSourced.Domain;
+
+namespace EventSourced.Snapshots
+{
+    public interface ISnapshotCreationStrategy
+    {
+        bool ShouldCreateSnapshot<TAggregateRoot>(TAggregateRoot root)
+            where TAggregateRoot : AggregateRoot;
+    }
+}
