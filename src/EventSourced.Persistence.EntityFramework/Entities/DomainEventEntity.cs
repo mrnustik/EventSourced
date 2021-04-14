fef75dc6c9614d692e14a399114b8856b0fe1b1a
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-14 22:05:02 +0200

    Add DbContext for EntityFramework storing

---
+++ src/EventSourced.Persistence.EntityFramework/Entities/DomainEventEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EventSourced.Persistence.EntityFramework.Entities
+{
+    public class DomainEventEntity
+    {
+        public Guid Id { get; set; }
+        public string EventType { get; set; } = null!;
+        public Guid StreamId { get; set; }
+        public string AggregateRootType { get; set; } = null!;
+        public string SerializedEvent { get; set; } = null!;
+    }
+}
