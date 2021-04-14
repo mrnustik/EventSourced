fc2f4bfe2b916d7380b23899d6648c71ed94df44
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-14 09:40:25 +0200

    Add automatic projection rebuilding on startup

--- src/EventSourced/Configuration/AutomaticProjectionOptions.cs
+++ src/EventSourced/Configuration/AutomaticProjectionOptions.cs
@@ -6,5 +6,6 @@ namespace EventSourced.Configuration
     public class AutomaticProjectionOptions
     {
         public ICollection<Type> RegisteredAutomaticProjections { get; } = new List<Type>();
+        public bool RebuildAutomaticProjectionsOnStart { get; internal set; } = false;
     }
 }
---
+++ src/EventSourced/Projections/Automatic/IAutomaticProjectionRebuilder.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventSourced.Projections.Automatic
+{
+    public interface IAutomaticProjectionRebuilder
+    {
+        Task RebuildAllRegisteredAutomaticProjections(CancellationToken ct);
+    }
+}
