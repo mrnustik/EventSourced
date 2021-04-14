373e3207309deca05f207d8ebd053fbbf8d43b4b
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-14 22:06:01 +0200

    Remove Id from domain event

--- src/EventSourced/Domain/Events/IDomainEvent.cs
+++ src/EventSourced/Domain/Events/IDomainEvent.cs
@@ -1,10 +1,7 @@
-﻿using System;
-
-namespace EventSourced.Domain.Events
+﻿namespace EventSourced.Domain.Events
 {
     public interface IDomainEvent
     {
-        Guid Id { get; }
         int Version { get; set; }
     }
 }
