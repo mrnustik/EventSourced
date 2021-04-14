5f8d4a5799dbcd3ceb4209ec929304910f632954
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-14 22:05:02 +0200

    Add DbContext for EntityFramework storing

---
+++ src/EventSourced.Persistence.EntityFramework/EventSourcedDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EventSourced.Persistence.EntityFramework.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventSourced.Persistence.EntityFramework
+{
+    public class EventSourcedDbContext : DbContext
+    {
+        public DbSet<DomainEventEntity> Events { get; set; } = null!;
+        
+        public EventSourcedDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+    }
+}
