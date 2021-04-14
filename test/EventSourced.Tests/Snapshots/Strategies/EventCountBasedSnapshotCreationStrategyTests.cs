31d48496753a8a45311b0f7c6f4dd7f707ef000c
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-14 18:36:25 +0200

    Add event count based snapshot creation strategy

---
+++ test/EventSourced.Tests/Snapshots/Strategies/EventCountBasedSnapshotCreationStrategyTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using EventSourced.Domain;
+using EventSourced.Snapshots;
+using EventSourced.Snapshots.Strategies;
+using FluentAssertions;
+using Xunit;
+
+namespace EventSourced.Tests.Snapshots.Strategies
+{
+    public class EventCountBasedSnapshotCreationStrategyTests
+    {
+        [Fact]
+        public void ShouldCreateSnapshot_WhenAggregateVersionMatches_ReturnsTrue()
+        {
+            //Arrange
+            var aggregateRoot = new TestAggregateRoot(Guid.NewGuid());
+            aggregateRoot.SetVersion(5);
+            var sut = CreateSut(5);
+            
+            //Act
+            var shouldCreateSnapshot = sut.ShouldCreateSnapshot(aggregateRoot);
+
+            //Assert
+            shouldCreateSnapshot.Should()
+                                .BeTrue();
+        }
+        
+        [Fact]
+        public void ShouldCreateSnapshot_WhenAggregateVersionDoesNotMatch_ReturnsFalse()
+        {
+            //Arrange
+            var aggregateRoot = new TestAggregateRoot(Guid.NewGuid());
+            aggregateRoot.SetVersion(4);
+            var sut = CreateSut(5);
+            
+            //Act
+            var shouldCreateSnapshot = sut.ShouldCreateSnapshot(aggregateRoot);
+
+            //Assert
+            shouldCreateSnapshot.Should()
+                                .BeFalse();
+        }
+
+        private ISnapshotCreationStrategy CreateSut(int eventCountBetweenSnapshots)
+        {
+            return new EventCountBasedSnapshotCreationStrategy(eventCountBetweenSnapshots);
+        }
+        
+        private class TestAggregateRoot : AggregateRoot
+        {
+            public TestAggregateRoot(Guid id)
+                : base(id)
+            {
+            }
+
+            public void SetVersion(int version)
+            {
+                Version = version;
+            }
+        }
+    }
+}
