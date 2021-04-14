c201aa01759daa40c00949cce71c01dd584a003b
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-14 22:05:44 +0200

    Add migration handling hosted service

---
+++ src/EventSourced.Persistence.EntityFramework/EventSourcedMigrationHostedService.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using EventSourced.Helpers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EventSourced.Persistence.EntityFramework
+{
+    public class EventSourcedMigrationHostedService : HostedServiceBase
+    {
+        public EventSourcedMigrationHostedService(IServiceScopeFactory serviceScopeFactory)
+            : base(serviceScopeFactory)
+        {
+        }
+
+        protected override async Task StartAsync(IServiceScope serviceScope, CancellationToken cancellationToken)
+        {
+            var eventSourcedDbContext = serviceScope.ServiceProvider.GetRequiredService<EventSourcedDbContext>();
+            // await eventSourcedDbContext.Database.EnsureDeletedAsync(cancellationToken);
+            await eventSourcedDbContext.Database.EnsureCreatedAsync(cancellationToken);
+        }
+    }
+}
