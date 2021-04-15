3cc76f17a69f060663ba3fd65038e45766318368
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-15 10:04:12 +0200

    Add updating of title to warehouse list item

---
+++ samples/EventSourced.Sample.Warehouse.Application/Services/WarehouseItem/UpdateWarehouseItemApplicationService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using EventSourced.Persistence;
+using EventSourced.Sample.Warehouse.Domain.WarehouseItem;
+
+namespace EventSourced.Sample.Warehouse.Application.Services.WarehouseItem
+{
+    public interface IUpdateWarehouseItemApplicationService
+    {
+        Task UpdateAsync(Guid warehouseItemId, string title, CancellationToken ct);
+    }
+
+    public class UpdateWarehouseItemApplicationService : ApplicationServiceBase, IUpdateWarehouseItemApplicationService
+    {
+        private readonly IRepository<WarehouseItemAggregateRoot> _warehouseItemRepository;
+
+        public UpdateWarehouseItemApplicationService(IRepository<WarehouseItemAggregateRoot> warehouseItemRepository)
+        {
+            _warehouseItemRepository = warehouseItemRepository;
+        }
+
+        public async Task UpdateAsync(Guid warehouseItemId, string title, CancellationToken ct)
+        {
+            var warehouseItem = await _warehouseItemRepository.GetByIdAsync(warehouseItemId, ct);
+            warehouseItem.UpdateTitle(title);
+            await _warehouseItemRepository.SaveAsync(warehouseItem, ct);
+        }
+    }
+}
---
+++ samples/EventSourced.Sample.Warehouse.Domain/WarehouseItem/Events/WarehouseItemTitleUpdatedDomainEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using EventSourced.Domain.Events;
+
+namespace EventSourced.Sample.Warehouse.Domain.WarehouseItem.Events
+{
+    public class WarehouseItemTitleUpdatedDomainEvent : DomainEvent
+    {
+        public Guid WarehouseItemId { get; }
+        public string NewTitle { get; }
+
+        public WarehouseItemTitleUpdatedDomainEvent(Guid warehouseItemId, string newTitle)
+        {
+            NewTitle = newTitle;
+            WarehouseItemId = warehouseItemId;
+        }
+    }
+}
