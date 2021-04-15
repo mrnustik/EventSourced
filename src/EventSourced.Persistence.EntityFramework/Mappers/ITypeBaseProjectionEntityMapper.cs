fae63a3390ec169ae7fcd622bc26af3cb8054b4e
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-15 10:46:23 +0200

    Move interfaces to its own files

---
+++ src/EventSourced.Persistence.EntityFramework/Mappers/ITypeBaseProjectionEntityMapper.cs
@@ -0,0 +1,10 @@
+﻿using EventSourced.Persistence.EntityFramework.Entities;
+
+namespace EventSourced.Persistence.EntityFramework.Mappers
+{
+    public interface ITypeBaseProjectionEntityMapper
+    {
+        TypeBasedProjectionEntity MapToEntity(object projection);
+        object MapToProjection(TypeBasedProjectionEntity entity);
+    }
+}
