f823336dc297f97d867638f273777a3a1d1815a3
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-15 10:46:23 +0200

    Move interfaces to its own files

--- src/EventSourced.Persistence.EntityFramework/Mappers/TypeBaseProjectionEntityMapper.cs
+++ src/EventSourced.Persistence.EntityFramework/Mappers/TypeBaseProjectionEntityMapper.cs
@@ -4,12 +4,6 @@ using Newtonsoft.Json;
 
 namespace EventSourced.Persistence.EntityFramework.Mappers
 {
-    public interface ITypeBaseProjectionEntityMapper
-    {
-        TypeBasedProjectionEntity MapToEntity(object projection);
-        object MapToProjection(TypeBasedProjectionEntity entity);
-    }
-
     public class TypeBaseProjectionEntityMapper : ITypeBaseProjectionEntityMapper
     {
         private readonly ITypeSerializer _typeSerializer;
