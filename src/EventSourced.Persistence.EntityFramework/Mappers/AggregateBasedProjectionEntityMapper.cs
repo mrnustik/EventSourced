0071e32816e90119e80824f066e1a80b3a8984b3
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-15 10:46:23 +0200

    Move interfaces to its own files

--- src/EventSourced.Persistence.EntityFramework/Mappers/AggregateBasedProjectionEntityMapper.cs
+++ src/EventSourced.Persistence.EntityFramework/Mappers/AggregateBasedProjectionEntityMapper.cs
@@ -6,12 +6,6 @@ using Newtonsoft.Json;
 
 namespace EventSourced.Persistence.EntityFramework.Mappers
 {
-    public interface IAggregateBasedProjectionEntityMapper
-    {
-        AggregateBasedProjectionEntity MapToEntity(Guid aggregateRootId, object projection);
-        object MapToProjection(AggregateBasedProjectionEntity entity);
-    }
-
     public class AggregateBasedProjectionEntityMapper : IAggregateBasedProjectionEntityMapper
     {
         private readonly ITypeSerializer _typeSerializer;
---
+++ src/EventSourced.Persistence.EntityFramework/Mappers/IAggregateBasedProjectionEntityMapper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using EventSourced.Persistence.EntityFramework.Entities;
+
+namespace EventSourced.Persistence.EntityFramework.Mappers
+{
+    public interface IAggregateBasedProjectionEntityMapper
+    {
+        AggregateBasedProjectionEntity MapToEntity(Guid aggregateRootId, object projection);
+        object MapToProjection(AggregateBasedProjectionEntity entity);
+    }
+}
