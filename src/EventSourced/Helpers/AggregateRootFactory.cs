bd4e68ec3b15fc66b6617acabcb186b35942077d
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-16 11:41:26 +0200

    Add viewing of json aggregate data

---
+++ src/EventSourced/Helpers/AggregateRootFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using EventSourced.Domain;
+
+namespace EventSourced.Helpers
+{
+    public class AggregateRootFactory
+    {
+        public static TAggregateRoot CreateAggregateRoot<TAggregateRoot>(Guid id)
+            where TAggregateRoot : AggregateRoot
+            => (TAggregateRoot) CreateAggregateRoot(id, typeof(TAggregateRoot));
+        
+        public static AggregateRoot CreateAggregateRoot(Guid id, Type type)
+        {
+            var aggregateRoot = (AggregateRoot) Activator.CreateInstance(type, id)!;
+            return aggregateRoot;
+        }
+    }
+}
