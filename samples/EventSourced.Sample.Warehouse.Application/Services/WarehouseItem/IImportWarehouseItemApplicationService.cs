e1ad9c6a1aa412004b012d6af8dc163696665a13
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-16 20:22:25 +0200

    Add importing of warehouse items via warehouse item detail

---
+++ samples/EventSourced.Sample.Warehouse.Application/Services/WarehouseItem/IImportWarehouseItemApplicationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventSourced.Sample.Warehouse.Application.Services.WarehouseItem
+{
+    public interface IImportWarehouseItemApplicationService
+    {
+        Task ImportWarehouseItemAsync(Guid warehouseItemId, int amount, CancellationToken ct);
+    }
+}
