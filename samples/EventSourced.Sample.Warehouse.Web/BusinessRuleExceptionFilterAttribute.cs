de9056e3a2f38c5d21250716f7042f9d3397981b
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-16 20:31:06 +0200

    Add validation messages when importing of items fails

---
+++ samples/EventSourced.Sample.Warehouse.Web/BusinessRuleExceptionFilterAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using DotVVM.Framework.Hosting;
+using DotVVM.Framework.Runtime.Filters;
+using DotVVM.Framework.ViewModel.Validation;
+using EventSourced.Sample.Warehouse.Domain.Exceptions;
+
+namespace EventSourced.Sample.Warehouse.Web
+{
+    public class BusinessRuleExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public string ErrorItemPath { get; }
+
+        public BusinessRuleExceptionFilterAttribute(string errorItemPath)
+        {
+            ErrorItemPath = errorItemPath;
+        }
+
+        protected override Task OnCommandExceptionAsync(IDotvvmRequestContext context, ActionInfo actionInfo, Exception ex)
+        {
+            if (ex is BusinessRuleException businessRuleException)
+            {
+                context.ModelState.Errors.Add(new ViewModelValidationError
+                {
+                    ErrorMessage = businessRuleException.Message,
+                    PropertyPath = ErrorItemPath,
+                });
+                context.IsCommandExceptionHandled = true;
+                
+                context.FailOnInvalidModelState();
+            }
+            return base.OnCommandExceptionAsync(context, actionInfo, ex);
+        }
+    }
+}
