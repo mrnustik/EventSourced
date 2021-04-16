55d26a20f79910cd4d398b40baf369119005d1c6
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-16 18:51:19 +0200

    Add warehouse item list page

---
+++ samples/EventSourced.Sample.Warehouse.Web/Pages/Items/List/ListViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+using EventSourced.Sample.Warehouse.Application.Model;
+using EventSourced.Sample.Warehouse.Application.Services.WarehouseItem;
+
+namespace EventSourced.Sample.Warehouse.Web.Pages.Items.List
+{
+    public class ListViewModel : MasterPageViewModel
+    {
+        private readonly IGetAllWarehouseItemsApplicationService _getAllWarehouseItemsApplicationService;
+
+        public ListViewModel(IGetAllWarehouseItemsApplicationService getAllWarehouseItemsApplicationService)
+        {
+            _getAllWarehouseItemsApplicationService = getAllWarehouseItemsApplicationService;
+        }
+
+        [Bind(Direction.ServerToClient)]
+        public ICollection<WarehouseLisItemModel> WarehouseListItems { get; set; } = new List<WarehouseLisItemModel>();
+
+        public override async Task PreRender()
+        {
+            await base.PreRender();
+            if (!Context.IsPostBack)
+            {
+                WarehouseListItems = await _getAllWarehouseItemsApplicationService.GetAllAsync(RequestCancellationToken);
+            }
+        }
+    }
+}
