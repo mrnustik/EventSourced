dde3680cb948e587d250e21cd561035949012a3a
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-16 12:08:52 +0200

    Add visualization of aggregate data via JsonViewer control

---
+++ src/EventSourced.Diagnostics.Web/Resources/JsonViewer.js
@@ -0,0 +1,8 @@
+﻿ko.bindingHandlers["JsonViewer"] = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+    },
+    update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        const json = ko.unwrap(valueAccessor().Json)
+        $(element).jsonViewer(JSON.parse(json));
+    }
+};
