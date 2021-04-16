42817480b370fcb46b462c25271f91b5e6d4ef50
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-16 19:12:16 +0200

    Add warehouse item editting page

---
+++ samples/EventSourced.Sample.Warehouse.Web/Pages/Items/Edit/EditViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+using EventSourced.Sample.Warehouse.Application.Services.WarehouseItem;
+
+namespace EventSourced.Sample.Warehouse.Web.Pages.Items.Edit
+{
+    public class EditViewModel : MasterPageViewModel
+    {
+        private readonly IWarehouseItemDetailApplicationService _warehouseItemDetailApplicationService;
+        private readonly IUpdateWarehouseItemApplicationService _updateWarehouseItemApplicationService;
+        private readonly ICreateWarehouseItemApplicationService _createWarehouseItemApplicationService;
+
+        public EditViewModel(IWarehouseItemDetailApplicationService warehouseItemDetailApplicationService,
+                             IUpdateWarehouseItemApplicationService updateWarehouseItemApplicationService,
+                             ICreateWarehouseItemApplicationService createWarehouseItemApplicationService)
+        {
+            _warehouseItemDetailApplicationService = warehouseItemDetailApplicationService;
+            _updateWarehouseItemApplicationService = updateWarehouseItemApplicationService;
+            _createWarehouseItemApplicationService = createWarehouseItemApplicationService;
+        }
+
+        [FromRoute(nameof(WarehouseItemId))]
+        public Guid? WarehouseItemId { get; set; }
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        public override async Task PreRender()
+        {
+            await base.PreRender();
+            if (WarehouseItemId.HasValue)
+            {
+                var existingItem =
+                    await _warehouseItemDetailApplicationService.GetWarehouseItemDetailAsync(
+                        WarehouseItemId.Value,
+                        RequestCancellationToken);
+                Title = existingItem.Title;
+            }
+        }
+
+        public async Task SaveAsync()
+        {
+            if (WarehouseItemId.HasValue)
+            {
+                await _updateWarehouseItemApplicationService.UpdateAsync(WarehouseItemId.Value, Title, RequestCancellationToken);
+            }
+            else
+            {
+                await _createWarehouseItemApplicationService.CreateWarehouseItemAsync(Title, RequestCancellationToken);
+            }
+            Context.RedirectToRoute("Pages/Items/List/List");
+        }
+    }
+}
