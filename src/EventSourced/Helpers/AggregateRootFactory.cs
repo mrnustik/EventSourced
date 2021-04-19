5a3fd9dc4ed61d6cc6590778b4bfcfb2b42efed1
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-19 11:41:39 +0200

    Add non public constructor support to AggregateRoot

--- src/EventSourced/Helpers/AggregateRootFactory.cs
+++ src/EventSourced/Helpers/AggregateRootFactory.cs
@@ -1,18 +1,28 @@
 ﻿using System;
 using EventSourced.Domain;
+using BindingFlags = System.Reflection.BindingFlags;
 
 namespace EventSourced.Helpers
 {
     public class AggregateRootFactory
     {
-        public static TAggregateRoot CreateAggregateRoot<TAggregateRoot>(Guid id)
-            where TAggregateRoot : AggregateRoot
-            => (TAggregateRoot) CreateAggregateRoot(id, typeof(TAggregateRoot));
-        
+        public static TAggregateRoot CreateAggregateRoot<TAggregateRoot>(Guid id) where TAggregateRoot : AggregateRoot =>
+            (TAggregateRoot) CreateAggregateRoot(id, typeof(TAggregateRoot));
+
         public static AggregateRoot CreateAggregateRoot(Guid id, Type type)
         {
-            var aggregateRoot = (AggregateRoot) Activator.CreateInstance(type, id)!;
-            return aggregateRoot;
+            var constructorInfo = type.GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public,
+                                                      null,
+                                                      new[] {typeof(Guid)},
+                                                      null);
+            if (constructorInfo == null)
+            {
+                throw new ArgumentException($"Could not find constructor accepting a Guid on aggregate of type {type.FullName}");
+            }
+            else
+            {
+                return (AggregateRoot) constructorInfo.Invoke(new object?[] {id});
+            }
         }
     }
 }
