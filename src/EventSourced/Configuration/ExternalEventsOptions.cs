aad62b35ed73a7a909e0102e322de757972ea5cf
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-20 21:01:19 +0200

    Add external event publishing to EventSourced project

---
+++ src/EventSourced/Configuration/ExternalEventsOptions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventSourced.Configuration
+{
+    public class ExternalEventsOptions
+    {
+        public ICollection<Type> RegisteredExternalEvents { get; } = new List<Type>();
+    }
+}
---
+++ src/EventSourced/Exceptions/ExternalEventNotRegisteredException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace EventSourced.Exceptions
+{
+    [Serializable]
+    public class ExternalEventNotRegisteredException : Exception
+    {
+        public ExternalEventNotRegisteredException(string? message)
+            : base(message)
+        {
+        }
+        
+        protected ExternalEventNotRegisteredException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
