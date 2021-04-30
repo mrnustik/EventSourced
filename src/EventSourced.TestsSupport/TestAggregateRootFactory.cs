73f47846bab6b731b1e632307b9aaf7f1e878884
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-30 14:19:05 +0200

    Add TestAggregateRootFactory

---
+++ src/EventSourced.TestsSupport/TestAggregateRootFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using EventSourced.Domain;
+using EventSourced.Domain.Events;
+using EventSourced.Helpers;
+using Newtonsoft.Json;
+
+namespace EventSourced.TestsSupport
+{
+    public static class TestAggregateRootFactory
+    {
+        public static TAggregateRoot CreateAggregateFromEvents<TAggregateRoot>(Guid id, string eventsArrayJson)
+            where TAggregateRoot : AggregateRoot
+        {
+            var aggregateRoot = AggregateRootFactory.CreateAggregateRoot<TAggregateRoot>(id);
+            var events = JsonConvert.DeserializeObject<IEnumerable<DomainEvent>>(
+                eventsArrayJson,
+                new JsonSerializerSettings {TypeNameHandling = TypeNameHandling.Auto});
+            if (events == null)
+            {
+                throw new ArgumentException($"The factory was unable to deserialize events from json {eventsArrayJson}");
+            }
+            aggregateRoot.RebuildFromEvents(events);
+            return aggregateRoot;
+        }
+        
+        public static TAggregateRoot CreateAggregateFromState<TAggregateRoot>(Guid id , string aggregateStateJson)
+            where TAggregateRoot : AggregateRoot
+        {
+            var aggregateRoot = AggregateRootFactory.CreateAggregateRoot<TAggregateRoot>(id);
+            JsonConvert.PopulateObject(aggregateStateJson, aggregateRoot);
+            return aggregateRoot;
+        } 
+    }
+}
