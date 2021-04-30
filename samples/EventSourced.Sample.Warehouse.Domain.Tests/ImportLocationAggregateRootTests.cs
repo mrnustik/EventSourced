58f9288d8a739c410823003816f70197d2449a43
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-30 14:19:22 +0200

    Refactor ImportLocationAggregateRootTests to use TestSupport library

--- samples/EventSourced.Sample.Warehouse.Domain.Tests/ImportLocationAggregateRootTests.cs
+++ samples/EventSourced.Sample.Warehouse.Domain.Tests/ImportLocationAggregateRootTests.cs
@@ -2,6 +2,7 @@ using System;
 using EventSourced.Helpers;
 using EventSourced.Sample.Warehouse.Domain.Exceptions;
 using EventSourced.Sample.Warehouse.Domain.ImportLocation;
+using EventSourced.TestsSupport;
 using FluentAssertions;
 using Newtonsoft.Json;
 using Xunit;
@@ -16,8 +17,7 @@ namespace EventSourced.Sample.Warehouse.Domain.Tests
             //Arrange
             var aggregateState =
                 @"{""ImportedItems"":[{""WarehouseItemId"":""4e52e08d-2282-45bd-a480-e8775c12589b"",""Amount"":25},{""WarehouseItemId"":""4af48b93-7c96-49d0-b055-f19380636563"",""Amount"":8},{""WarehouseItemId"":""0ab33a01-8001-452e-9114-b207de6b8122"",""Amount"":40}],""Version"":10,""Id"":""3de7b3d9-0c35-4cd3-8dea-e29baea0f8af""}";
-            var importLocationAggregateRoot = AggregateRootFactory.CreateAggregateRoot<ImportLocationAggregateRoot>(new Guid("3de7b3d9-0c35-4cd3-8dea-e29baea0f8af"));
-            JsonConvert.PopulateObject(aggregateState, importLocationAggregateRoot);
+            var importLocationAggregateRoot = TestAggregateRootFactory.CreateAggregateFromState<ImportLocationAggregateRoot>(new Guid("3de7b3d9-0c35-4cd3-8dea-e29baea0f8af"), aggregateState);
             
             //Act
             Action action = () => importLocationAggregateRoot.MoveItem(new Guid("4af48b93-7c96-49d0-b055-f19380636563"), 10);
