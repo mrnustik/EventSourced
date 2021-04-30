5e21ab8aead368d6ef5a44cbd8757e985d4bb2ac
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-04-30 17:25:54 +0200

    Add authorization to EventSourced diagnostics

--- src/EventSourced.Diagnostics.Web/Pages/ViewModelBase.cs
+++ src/EventSourced.Diagnostics.Web/Pages/ViewModelBase.cs
@@ -1,10 +1,32 @@
 ﻿using System.Threading;
+using System.Threading.Tasks;
 using DotVVM.Framework.ViewModel;
+using EventSourced.Diagnostics.Web.Configuration;
+using Microsoft.AspNetCore.Http;
 
 namespace EventSourced.Diagnostics.Web.Pages
 {
     public class ViewModelBase : DotvvmViewModelBase
     {
+        private readonly EventSourcedDiagnosticsOptions _options;
+
+        public ViewModelBase(EventSourcedDiagnosticsOptions options)
+        {
+            _options = options;
+        }
+        
         public CancellationToken RequestCancellationToken => Context.GetCancellationToken();
+
+        public override async Task Init()
+        {
+            if (!await _options.Authorize(Context))
+            {
+                var response = Context.HttpContext.Response;
+                response.StatusCode = 403;
+
+                Context.InterruptRequest();
+            }
+            await base.Init();
+        }
     }
 }
