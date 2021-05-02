17af8345536c29ca981eeaf0359d40768018f8ff
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-05-02 16:00:36 +0200

    Move getting of warehouse item title to domain service

---
+++ samples/EventSourced.Sample.Warehouse.Domain/WarehouseItem/Services/WarehouseItemTitleService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using EventSourced.Persistence;
+using EventSourced.Sample.Warehouse.Domain.Exceptions;
+using EventSourced.Sample.Warehouse.Domain.WarehouseItem.Projections;
+
+namespace EventSourced.Sample.Warehouse.Domain.WarehouseItem.Services
+{
+    class WarehouseItemTitleService : IWarehouseItemTitleService
+    {
+        private readonly IProjectionStore _projectionStore;
+
+        public WarehouseItemTitleService(IProjectionStore projectionStore)
+        {
+            _projectionStore = projectionStore;
+        }
+
+        public async Task<string> GetWarehouseItemTitleAsync(Guid warehouseItemId, CancellationToken ct)
+        {
+            var warehouseItem = await _projectionStore.LoadAggregateProjectionAsync<WarehouseItemDetailProjection, WarehouseItemAggregateRoot>(
+                warehouseItemId,
+                ct);
+            if (warehouseItem == null)
+            {
+                throw new BusinessRuleException($"Warehouse item with id {warehouseItemId} was not found");
+            }
+            return warehouseItem.Title;
+        }
+    }
+}
