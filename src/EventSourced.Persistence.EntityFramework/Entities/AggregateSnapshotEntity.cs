0bc9c65fdc9a45c281125c9cd3a34821bfe2084f
Michal Mrnuštík <michal.mrnustik@outlook.com>
2021-05-02 17:23:53 +0200

    Rename columns in EntityFramework implementation to match contents

--- src/EventSourced.Persistence.EntityFramework/Entities/AggregateSnapshotEntity.cs
+++ src/EventSourced.Persistence.EntityFramework/Entities/AggregateSnapshotEntity.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace EventSourced.Persistence.EntityFramework.Entities
 {
     public class AggregateSnapshotEntity
     {
-        public Guid Id { get; set; }
+        [Key]
+        public Guid AggregateRootId { get; set; }
         public int Version { get; set; }
         public string AggregateRootType { get; set; } = null!;
         public string SerializedAggregateState { get; set; } = null!;
